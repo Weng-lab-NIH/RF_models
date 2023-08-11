0b1536669ce67009c7adca576d8d5511bef547c1
Syed Abbas Bukhari <sbukhar@illinois.edu>
2023-08-10 21:58:12 -0500

    RF_models.sh
    a bug is fixed, now the number of bins will be made by considering the tcr size differences of positive and negative training sets.

--- RF_models.sh
+++ RF_models.sh
@@ -6,12 +6,12 @@ if [ "$1" == "-h" ]; then
   echo "Usage: `basename $0` [-h]"
   echo "RF_models.sh takes five arguments"
   echo "See example script below:"
-  echo "./RF_models.sh train_posset.csv train_negset.csv testset.csv RF_models_workingdir_name cdr3only[true/fasle]"
-  echo "train_posset.csv ==> file name of postive training set"
-  echo "train_negset.csv ==> file name of negative training set"
-  echo "RF_models_workingdir_name ==> working directory where intermediary and results file will be stored. "
-  echo "cdr3only[true/fasle] ==> if true then pipeline will be run for cdr3 region only, if false then pipeline will be run for all cdr (1,2,3) regions"
-  echo "RF predictions will be stored in RF_models_workingdir_name/RFtestingset_scores.txt"
+  echo "\t\t./RF_models.sh train_posset.csv train_negset.csv testset.csv RF_models_workingdir_name cdr3only[true/fasle]"
+  echo "\t\ttrain_posset.csv ==> file name of postive training set"
+  echo "\t\ttrain_negset.csv ==> file name of negative training set"
+  echo "\t\tRF_models_workingdir_name ==> working directory where intermediary and results file will be stored. "
+  echo "\t\tcdr3only[true/fasle] ==> if true then pipeline will be run for cdr3 region only, if false then pipeline will be run for all cdr (1,2,3) regions"
+  echo "\t\tRF predictions will be stored in RF_models_workingdir_name/RFtestingset_scores.txt"
   exit 0
 fi
 
@@ -42,8 +42,12 @@ if [ "$cdr3_only" == "true" ]; then
         python ../RF_models/_1b_remove_chains.py loc_3mer.p neg_3mer.npy models_neg/
 
         mkdir bins
-
-        for i in {0..9};
+	nline_pos=$(wc -l < $pos)
+	nlines_neg=$(wc -l < $neg)
+	echo $((nlines_neg / nline_pos))
+	n_bins=$((nlines_neg / nline_pos))
+	n_bins=$(($n_bins))
+        for i in $(seq $n_bins);
         do
         date
         python ../RF_models/_3_random_forest.py \
@@ -74,16 +78,18 @@ if [ "$cdr3_only" == "true" ]; then
 
         #run prediction script using RF trees created in the earlier step. 
 
-        python ../RF_models/_4_apply_split_RFs.py $PWD/bins model_trees $PWD/models_test/only_cd3r_obj.npy RFtestingset_scores.txt
+        python ../RF_models/_4_apply_split_RFs.py $PWD/bins model_trees $PWD/models_test/only_cd3r_obj.npy $4_RFtestingset_scores.txt
 
 fi
 
 if [ "$cdr3_only" == "false" ] ; then
+	echo "creating $4_allcdrs"
         dir="$4_allcdrs"
         if [ -d "$dir" ]; then rm -Rf $dir; fi
         mkdir $dir
 
         #step 1
+	echo "step 1, creating python object using input data"
         cd $dir
         date
         python ../RF_models/_1_create_kmer_dicts1.py generate loc_3mer.p \
@@ -91,10 +97,15 @@ if [ "$cdr3_only" == "false" ] ; then
                 --output_names pos_3mer neg_3mer \
                 --length_dependent_trimming True \
                 --kmer_size 3
-
+	
+	echo "step 2, making bins, each bin will have training tree"
         mkdir bins
-
-        for i in {0..9};
+ 	nline_pos=$(wc -l < $pos)
+        nlines_neg=$(wc -l < $neg)
+        echo $((nlines_neg / nline_pos))
+        n_bins=$((nlines_neg / nline_pos))
+	n_bins=$(($n_bins))
+	for i in $(seq $n_bins);
         do
         date
         python ../RF_models/_3_random_forest.py \
@@ -109,7 +120,8 @@ if [ "$cdr3_only" == "false" ] ; then
                 --model_name ./bins/model_trees_$i.p
 
         done
-
+	
+	echo "creating python object of the testing set."
         #create python object of the testing set. 
 
         python ../RF_models/_1_create_kmer_dicts1.py encode loc_3mer.p \
@@ -119,9 +131,10 @@ if [ "$cdr3_only" == "false" ] ; then
                 --length_dependent_trimming True \
                 --kmer_size 3
 
+	echo "running prediction script using RF trees created in step 2"
+
         #run prediction script using RF trees created in the earlier step. 
 
-        python ../RF_models/_4_apply_split_RFs.py $PWD/bins model_trees $PWD/RFtestingset.npy RFtestingset_scores.txt
+        python ../RF_models/_4_apply_split_RFs.py $PWD/bins model_trees $PWD/RFtestingset.npy $4_RFtestingset_scores.txt
 
 fi
-
